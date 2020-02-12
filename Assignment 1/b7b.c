da9d211341edb7b9a07a79881c4fd5846c11d89c
bas200o <bas.winkelhof@hotmail.nl>
2020-02-12 15:21:28 +0100

    b5+b7

---
+++ Assignment 1/b7b.c
@@ -0,0 +1,81 @@
+#define F_CPU 16E6
+
+#include <avr/io.h>
+#include <util/delay.h>
+#include <stdbool.h>
+
+
+
+void wait(int time) {
+	for (int i = 0; i < time; i++){
+		_delay_ms(1);
+	}
+}
+
+void main()
+{
+	DDRD = 0b11111111;
+	DDRC = 0b11111110;
+	char led = 0;
+	bool forward = false;
+	bool allLedsOn = false;
+	
+	while (1)
+	{
+		if (led == 0 || led == 7){
+			forward = !forward;
+		}
+		
+		if (forward)
+		{
+			led++;
+		}else{
+			led--;
+		}
+		
+		
+		if (led == 0)
+		{
+			DDRA = 0x00;
+		} 
+		else if (led == 1)
+		{
+			DDRA = 0b00000011;
+			PORTA = 0b00000001;
+		}
+		else if (led == 2)
+		{
+			DDRA = 0b00000011;
+			PORTA = 0b00000010;
+		}
+		else if (led == 3)
+		{
+			DDRA = 0b00000110;
+			PORTA = 0b00000010;
+		}
+		else if (led == 4)
+		{
+			DDRA = 0b00000110;
+			PORTA = 0b00000100;
+		}
+		else if (led == 5)
+		{
+			DDRA = 0b00000101;
+			PORTA = 0b00000100;
+		}		
+		else if (led == 6)
+		{
+			DDRA = 0b00000101;
+			PORTA = 0b00000001;
+		}	
+		
+		if (!allLedsOn)
+		{
+			wait(500);
+		}
+			
+				
+	}
+
+	return 1;
+}
